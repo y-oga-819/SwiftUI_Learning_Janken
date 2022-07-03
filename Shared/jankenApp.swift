b2c3e08513258145fa03f46113df32c56499368a
yuki819 <yuki819@mba2018.local>
2022-07-03 14:13:26 +0900

    Initial Commit

---
+++ Shared/jankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  jankenApp.swift
+//  Shared
+//
+//  Created by 小笠原佑樹 on 2022/07/03.
+//
+
+import SwiftUI
+
+@main
+struct jankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
