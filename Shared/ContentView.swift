82b18fa9686c8410edfe39c2b19ad88b8a9f29aa
yuki819 <yuki819@mba2018.local>
2022-07-04 00:15:56 +0900

    feature(Janken): テキストとボタンの余白をいい感じに調整する

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -18,6 +18,7 @@ struct ContentView: View {
             Spacer()
 
             Text("ぐー")
+                .padding(.bottom)
             Button(action: {
                 print("タップされたよ！")
             }) {
